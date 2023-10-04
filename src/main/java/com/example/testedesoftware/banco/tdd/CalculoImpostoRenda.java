d07a19022af0c197a122761f8be345ebd42f064c
George Ravelly <49557536+george-ravelly@users.noreply.github.com>
2023-10-03 22:50:58 -0300

    Update CalculoImpostoRenda.java

--- src/main/java/com/example/testedesoftware/banco/tdd/CalculoImpostoRenda.java
+++ src/main/java/com/example/testedesoftware/banco/tdd/CalculoImpostoRenda.java
@@ -3,42 +3,37 @@ package com.example.testedesoftware.banco.tdd;
 
 public class CalculoImpostoRenda {
 
-
     /**
-     * Este método calcula o imposto a ser pago baseado no valor de uma dada compra.
+     * Este método calcula o imposto de renda a ser pago com base no valor da renda anual.
      *
-     * Se  1201 <= valor <= 5000 deve pagar 10% de imposto
-     * Se  5001 <= valor <= 10000 deve pagar 15% de imposto
-     * Se  valor > 10000 deve pagar 20% de imposto
-     * Se  valor < 1201 deve pagar 0% de imposto
+     * Se 0 < renda <= 22847,76 deve pagar 0% de imposto
+     * Se 22847,77 <= renda <= 33919,80 deve pagar 7,5% de imposto
+     * Se 33919,81 <= renda <= 45012,60 deve pagar 15% de imposto
+     * Se 45012,61 <= renda <= 55976,16 deve pagar 22,5% de imposto
+     * Se renda > 55976,16 deve pagar 27,5% de imposto
      *
-     * @param valor - salario base para ser calculado o imposto de renda.
+     * @param renda - renda anual para calcular o imposto de renda.
      */
-    public static void calculaImposto(double valor) {
-        //Declaração de Variáveis
+    public static void calculaImposto(double renda) {
         double imposto;
-        double impostotot;
-        String porcento;
 
-        if (valor >= 1201 && valor <= 5000) {
-            imposto = 1.1;
-            porcento = "10%";
-            impostotot = 0.1;
-        } else if(valor >= 5001 && valor <= 10000){
-            imposto = 1.15;
-            porcento = "15%";
-            impostotot = 0.15;
-        } else{
-            imposto = 1.2;
-            porcento = "20%";
-            impostotot = 0.2;
+        if (renda <= 22847.76) {
+            imposto = 0;
+        } else if (renda <= 33919.80) {
+            imposto = (renda - 22847.76) * 0.075;
+        } else if (renda <= 45012.60) {
+            imposto = (renda - 33919.80) * 0.15 + 1100.43;
+        } else if (renda <= 55976.16) {
+            imposto = (renda - 45012.60) * 0.225 + 2826.21;
+        } else {
+            imposto = (renda - 55976.16) * 0.275 + 6172.13;
         }
-        //Saída de dados
-        System.out.println("Valor em Dinheiro: R$" + (valor*impostotot));
+
+        // Saída de dados
+        System.out.println("Imposto de Renda a ser pago: R$" + imposto);
     }
 
-
     public static void main(String[] args) {
-        calculaImposto(200.5);
+        calculaImposto(30000); // Exemplo de chamada com uma renda anual de 30.000 reais.
     }
 }
